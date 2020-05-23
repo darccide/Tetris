75ebe369be241726e85268d2faff7af725ad985e
darccide <darccide@protonmail.com>
2020-05-22 22:49:56 -0500

    mini grid and display shape functionality created

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
   const width = 10
+  let nextRandom = 0
 
   // Tetrominoes
   const lTetromino = [
@@ -94,7 +95,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
       // start a new tetromino falling
-      random = Math.floor(Math.random() * tetrominos.length)
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * tetrominos.length)
       current = tetrominos[random][currentRotation]
       currentPosition = 4
       draw()
@@ -139,4 +141,36 @@ document.addEventListener('DOMContentLoaded', () => {
     current = tetrominos[random][currentRotation]
     draw()
   }
+
+  // show up-next tetromino in mini-grid display
+  const displaySquares = document.querySelectorAll('.mini-grid div')
+  const displayWidth = 4
+  let displayIndex = 0
+
+
+  // the tetrominos without rotations
+  const upNextTetrominos = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2],  // lTetromino
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], // zTetromino
+    [1, displayWidth, displayWidth + 1, displayWidth + 2],  // tTetromino
+    [0, 1, displayWidth, displayWidth + 1], // oTetromino
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] // iTetromino
+  ]
+
+
+  // display the shape in the mini-grid display
+  function displayShape() {
+    // remove any trace of a tetromino from the entire shape
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+    })
+    upNextTetrominos[nextRandom].forEach(index => {
+      displaySquares[displayIndex + index].classList.add('tetromino')
+    })
+  }
+
+
+
+
+
 })
